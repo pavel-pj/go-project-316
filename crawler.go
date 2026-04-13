59bd10faf0fa30b3d603f0fd3dc982f63e9001ae
Pavel <voroshilov.pavel@gmail.com>
2026-04-13 17:01:17 +0300

    feat:some code

--- crawler.go
+++ crawler.go
@@ -4,11 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type Options struct {
 	URL        string
+	Depth      int32
 	HTTPClient *http.Client
 }
 
@@ -30,6 +35,15 @@ func Analyze(ctx context.Context, opts Options) ([]byte, error) {
 
 	defer resp.Body.Close()
 
+	// Читаем тело ответа
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, fmt.Errorf("Cant read response body from url:%s, %w", opts.URL, err)
+	}
+
+	links := getLinksFromHtml(bodyBytes)
+	fmt.Println(links)
+
 	response := Response{
 		URL:         opts.URL,
 		Http_status: resp.Status,
@@ -38,3 +52,52 @@ func Analyze(ctx context.Context, opts Options) ([]byte, error) {
 	return json.Marshal(response)
 
 }
+
+func getLinksFromHtml(htmlBytes []byte) []string {
+	// Преобразуем []byte в string
+	htmlString := string(htmlBytes)
+
+	// Парсим HTML
+	doc, err := html.Parse(strings.NewReader(htmlString))
+	if err != nil {
+		return []string{} // возвращаем пустой срез при ошибке
+	}
+
+	var rawLinks []string
+
+	var findLinks func(*html.Node)
+	findLinks = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					rawLinks = append(rawLinks, attr.Val)
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			findLinks(c)
+		}
+	}
+
+	findLinks(doc)
+
+	//удаляем дубликаты
+	links := uniqueStrings(rawLinks)
+
+	return links
+}
+
+func uniqueStrings(input []string) []string {
+	seen := make(map[string]bool)
+	result := []string{}
+
+	for _, val := range input {
+		if !seen[val] {
+			seen[val] = true
+			result = append(result, val)
+		}
+	}
+	return result
+}
+
+func checkPage()
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module code
 
 go 1.25.0
 
-require github.com/urfave/cli/v3 v3.8.0 // indirect
+require (
+	github.com/urfave/cli/v3 v3.8.0
+	golang.org/x/net v0.53.0
+)
